f998c920332aeb0572919c04e011448d2ad8cc0c
Manthan Vijay Barhate <manthanbarhate70@gmail.com>
2024-11-09 21:59:14 +0530

    Delete Backend directory

--- Backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { signup, login, signupRepresentative} = require('../controllers/authController');
-const router = express.Router();
-
-router.post('/signup', signup);
-router.post('/login', login);
-router.post('/signup-representative', signupRepresentative);
-
-module.exports = router;
